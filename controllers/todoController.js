adf5e385965146c22c806f0e348ead643aff0906
Joe Rehfuss <jrehfuss@c02g81atq05q.domain>
2022-12-02 10:30:51 -0800

    adding tokens

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {User,Todo} = require('../models');
+const jwt = require("jsonwebtoken")
 
 router.get("/",(req,res)=>{
     Todo.findAll().then(todoData=>{
@@ -25,6 +26,24 @@ router.get("/:id",(req,res)=>{
             err,
         })
     })
+})
+
+router.post("/",(req,res)=>{
+    try{
+        const token = req.headers.authorization.split(" ")[1];
+        const userData = jwt.verify(token,process.env.JWT_SECRET)
+        Todo.create({
+            task:req.body.task,
+            priority:req.body.priority,
+            isComplete:req.body.isComplete,
+            UserId:userData.id
+        }).then(todoData=>{
+            res.json(todoData)
+        })
+    }catch (err) {
+        console.log(err);
+        res.status(500).json({msg:"error occurred",err})
+    }
 })
 
 module.exports = router;
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 const {User,Todo} = require('../models');
 
 router.get("/",(req,res)=>{
@@ -24,6 +26,31 @@ router.get("/:id",(req,res)=>{
             msg:"an error occurred",
             err,
         })
+    })
+})
+
+router.post("/login",(req,res)=>{
+    User.findOne({
+        where:{
+            email:req.body.email
+        }
+    }).then(foundUser=>{
+        if(!foundUser){
+            return res.status(401).json({msg:"invalid login credentials"})
+        } else if(!bcrypt.compareSync(req.body.password,foundUser.password)){
+            return res.status(401).json({msg:"invalid login credentials"})
+        } else {
+            const token = jwt.sign({
+                id:foundUser.id,
+                email:foundUser.email
+            },process.env.JWT_SECRET,{
+                expiresIn:"2h"
+            })
+            return res.json({
+                token,
+                user:foundUser
+            })
+        }
     })
 })
 
