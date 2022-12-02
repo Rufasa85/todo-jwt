5f5b19ecbf2f75d1169957c9a455d90dc446a68c
Joe Rehfuss <jrehfuss@c02g81atq05q.domain>
2022-12-02 09:43:26 -0800

    updating port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const sequelize = require('./config/connection');
 // Sets up the Express App
 // =============================================================
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 // Requiring our models for syncing
 const { User,Todo} = require('./models');
 
